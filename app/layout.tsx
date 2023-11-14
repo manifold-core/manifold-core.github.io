2d191171b110a4fdc554365607cd7b39ec852e1d
Stan Liu <stanleyliu@berkeley.edu>
2023-11-14 13:39:43 -0600

    add hotjar script

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.svg" />
+      <script>
+        {`(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3735981,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </script>
       <body
         className={`${inter.variable} ${inter_tight.variable} font-inter bg-white tracking-tight text-zinc-900 antialiased`}
       >
